2b0b6778e8562ae3e32823e2964b391da002e869
ducksoup79 <flyingnut101@gmail.com>
2018-06-18 06:53:18 -0400

    added gitignore file

--- config/dbase_config.php
+++ config/dbase_config.php
@@ -1,18 +1,7 @@
 <?php
-<<<<<<< HEAD
-
 $dbhost = 'localhost';
 $dbname = 'sms';
 $dbuser = 'root';
 $dbpass = 'Nofear#79';
 
 $notification = 'so@safariair.co.bw';  //e-mail notification receiver
-=======
-$dbhost = 'localhost';
-$dbname = 'sms';
-$dbuser = 'root';
-$dbpass = 'nofear79';
-$notification = 'so@safariair.co.bw';
-
- ?>
->>>>>>> 81ddcce9ee9bcae6994e63c0074dc6f0e5a6899b
