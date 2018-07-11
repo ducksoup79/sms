255e2af11be4e87397052edc9897d24e8bf31ff0
ducksoup79 <flyingnut101@gmail.com>
2018-07-11 08:54:13 -0400

    changes made to delete

--- delete_hazard.php
+++ delete_hazard.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-require_once 'header.php';
-
-$id = $_GET['haz_num'];
-
-queryMysql("DELETE FROM hazard_reports WHERE hazard_num='$id'");
-queryMysql("DELETE FROM risk_assesment WHERE occurence_num='$id'");
-
-exit(header("Location:AllOpen.php"));
-=======
 <?php
 
 require_once 'header.php';
@@ -20,4 +8,3 @@ queryMysql("DELETE FROM hazard_reports WHERE hazard_num='$id'");
 queryMysql("DELETE FROM risk_assesment WHERE occurence_num='$id'");
 
 exit(header("Location:AllHazards.php"));
->>>>>>> 81ddcce9ee9bcae6994e63c0074dc6f0e5a6899b
